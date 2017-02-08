588abd5ff81785f4eb60a6ec6973375e113aa499
abgtrevize <abgtonline@gmail.com>
2017-02-08 17:23:48 -0600

    Test

--- scraper.py
+++ scraper.py
@@ -6,10 +6,10 @@ expected_string = "Hassle-free web scraping."
 
 # Check that proxy gives a working certificate for SSL connections
 # If the certificate isn't valid it should throw an exception
-html = scraperwiki.scrape("https://morph.io")
+#html = scraperwiki.scrape("https://morph.io")
 
-if not expected_string in html:
-    raise Exception("Not expected result")
+#if not expected_string in html:
+ #   raise Exception("Not expected result")
 
 # Use requests library to do the same because it gets its CA certs a different way. Oh joy.
 r = requests.get('https://morph.io')
