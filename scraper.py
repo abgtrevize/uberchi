1eb9a5320fd0430d25a124491267c2d4639c4c95
abgtrevize <abgtonline@gmail.com>
2017-02-08 19:51:16 -0600

    Test

--- scraper.py
+++ scraper.py
@@ -9,7 +9,7 @@ gps_Harper=[41.7889213, -87.5955699]
 productIDs='4bfc6c57-98c0-424f-a72e-c1e2a1d49939'
 
 #Uber with SDK
-accessToken=os.environ('MORPH_ACCESS_TOKEN')
+accessToken=os.environ['MORPH_ACCESS_TOKEN']
 
 url='https://api.uber.com/v1.2/products?latitude=%(1)s&longitude=%(2)s' % {'1':gps_Shoreham[0],'2':gps_Shoreham[1]}
 r=requests.get(url,headers={'Authorization':"Bearer %s" % accessToken})
