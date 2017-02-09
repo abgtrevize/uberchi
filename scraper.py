5177c74c92539d3d408e58659e3b963708a00c4e
abgtrevize <abgtonline@gmail.com>
2017-02-08 19:49:16 -0600

    Test

--- scraper.py
+++ scraper.py
@@ -9,25 +9,8 @@ gps_Harper=[41.7889213, -87.5955699]
 productIDs='4bfc6c57-98c0-424f-a72e-c1e2a1d49939'
 
 #Uber with SDK
-credentials = {
-	'access_token': os.environ['MORPH_ACCESS_TOKEN'],
-	'client_id': os.environ['MORPH_CLIENT_ID'],
-	'client_secret': os.environ['MORPH_CLIENT_SECRET'],
-	'expires_in_seconds': 2592000,  
-	'grant_type': 'authorization_code',
-	'scopes': None
-}
-oauth2_credential = OAuth2Credential(**credentials)
-session = Session(oauth2credential=oauth2_credential)
-client = UberRidesClient(session)
+accessToken=os.environ('MORPH_ACCESS_TOKEN')
 
-x_Shoreham_Harper=client.estimate_ride(
-	start_latitude=gps_Shoreham[0],
-	start_longitude=gps_Shoreham[1],
-	end_latitude=gps_Harper[0],
-	end_longitude=gps_Harper[1],
-	seat_count=2,
-	product_id=productIDs
-	)
-
-print x.json
+url='https://api.uber.com/v1.2/products?latitude=%(1)s&longitude=%(2)s' % {'1':gps_Shoreham[0],'2':gps_Shoreham[1]}
+r=requests.get(url,headers={'Authorization':"Bearer %s" % accessToken})
+print r.json()
