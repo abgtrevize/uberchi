383621b8e0c280bd0886e1a4fa852250ebe90992
abgtrevize <abgtonline@gmail.com>
2017-04-06 13:30:30 -0500

    Removed MPP and made routes dynamic

--- scraper.py
+++ scraper.py
@@ -30,10 +30,10 @@ def getPrice(start_latitude,start_longitude,end_latitude,end_longitude,productID
 	url='https://api.uber.com/v1.2/requests/estimate'
 	headers={'Authorization':"Bearer %s" % accessToken,"Content-Type": "application/json"}
 	params={
-		'start_latitude':gps_MPP.latitude,
-		'start_longitude':gps_MPP.longitude,
-		'end_latitude':gps_Harper.latitude,
-		'end_longitude':gps_Harper.longitude,
+		'start_latitude':start_latitude,
+		'start_longitude':start_longitude,
+		'end_latitude':end_latitude,
+		'end_longitude':end_longitude,
 		'seat_count':2,
 		'product_id':productIDs['uberX']}
 	requestx=requests.post(url,json=params,headers=headers).json()		
@@ -41,10 +41,10 @@ def getPrice(start_latitude,start_longitude,end_latitude,end_longitude,productID
 		unique_keys=['timestamp','product_id'],
 		data=dict(params,**{'price':requestx['fare']['value'],'timestamp':time.strftime('%Y-%m-%d %H:%M:%S')}))
 	params={
-		'start_latitude':gps_MPP.latitude,
-		'start_longitude':gps_MPP.longitude,
-		'end_latitude':gps_Harper.latitude,
-		'end_longitude':gps_Harper.longitude,
+		'start_latitude':start_latitude,
+		'start_longitude':start_longitude,
+		'end_latitude':end_latitude,
+		'end_longitude':end_longitude,
 		'seat_count':1,
 		'product_id':productIDs['uberPOOL']}
 	requestp1=requests.post(url,json=params,headers=headers).json()		
@@ -52,10 +52,10 @@ def getPrice(start_latitude,start_longitude,end_latitude,end_longitude,productID
 		unique_keys=['timestamp','product_id'],
 		data=dict(params,**{'price':requestp1['fare']['value'],'timestamp':time.strftime('%Y-%m-%d %H:%M:%S')}))
 	params={
-		'start_latitude':gps_MPP.latitude,
-		'start_longitude':gps_MPP.longitude,
-		'end_latitude':gps_Harper.latitude,
-		'end_longitude':gps_Harper.longitude,
+		'start_latitude':start_latitude,
+		'start_longitude':start_longitude,
+		'end_latitude':end_latitude,
+		'end_longitude':end_longitude,
 		'seat_count':2,
 		'product_id':productIDs['uberPOOL']}
 	requestp2=requests.post(url,json=params,headers=headers).json()		
@@ -66,32 +66,25 @@ def getPrice(start_latitude,start_longitude,end_latitude,end_longitude,productID
 def main():
 	#Get GPS Coords;
 	accessToken=os.environ['MORPH_ACCESS_TOKEN']
-	gps_MPP=getGPS('151 N Michigan Ave, Chicago, IL 60601');
 	gps_Shoreham=getGPS('400 East South Water St, Chicago, IL 60601');
 	gps_Harper=getGPS('5807 S Woodlawn Ave, Chicago, IL 60637');
 	productIDs=getProducts(accessToken,gps_Shoreham.latitude,gps_Shoreham.longitude)
 
 	while time.localtime().tm_hour<18:
 		if time.localtime().tm_hour<9:			
-			getPrice(productIDs,gps_MPP.latitude,gps_MPP.longitude,gps_Harper.latitude,gps_Harper.longitude)
 			getPrice(productIDs,gps_Shoreham.latitude,gps_Shoreham.longitude,gps_Harper.latitude,gps_Harper.longitude)
-			getPrice(productIDs,gps_Harper.latitude,gps_Harper.longitude,gps_MPP.latitude,gps_MPP.longitude)
 			getPrice(productIDs,gps_Harper.latitude,gps_Harper.longitude,gps_Shoreham.latitude,gps_Shoreham.longitude)
 			time.sleep(60*5)
 		elif time.localtime().tm_hour<11:
 			time.sleep(60*30)
 		elif time.localtime().tm_hour<14:
-			getPrice(productIDs,gps_MPP.latitude,gps_MPP.longitude,gps_Harper.latitude,gps_Harper.longitude)
 			getPrice(productIDs,gps_Shoreham.latitude,gps_Shoreham.longitude,gps_Harper.latitude,gps_Harper.longitude)
-			getPrice(productIDs,gps_Harper.latitude,gps_Harper.longitude,gps_MPP.latitude,gps_MPP.longitude)
 			getPrice(productIDs,gps_Harper.latitude,gps_Harper.longitude,gps_Shoreham.latitude,gps_Shoreham.longitude)
 			time.sleep(60*5)
 		elif time.localtime().tm_hour<16:
 			time.sleep(60.30)
 		else:
-			getPrice(productIDs,gps_MPP.latitude,gps_MPP.longitude,gps_Harper.latitude,gps_Harper.longitude)
 			getPrice(productIDs,gps_Shoreham.latitude,gps_Shoreham.longitude,gps_Harper.latitude,gps_Harper.longitude)
-			getPrice(productIDs,gps_Harper.latitude,gps_Harper.longitude,gps_MPP.latitude,gps_MPP.longitude)
 			getPrice(productIDs,gps_Harper.latitude,gps_Harper.longitude,gps_Shoreham.latitude,gps_Shoreham.longitude)
 			time.sleep(60*10);
 
